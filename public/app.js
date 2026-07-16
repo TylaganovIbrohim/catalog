6337944e60b1887fc452ada74c27d8367a015191
TylaganovIbrohim <kaispeedrungay@gmail.com>
2026-07-16 19:33:39 +0500

    Обновление сайта: боковое меню и кнопка О нас

--- public/app.js
+++ public/app.js
@@ -1,17 +1,6 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 
 import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: "AIzaSyAsrcT84CP6nZq8YG2KWfcu6TCtQpQoNm0",
@@ -23,19 +12,13 @@ const firebaseConfig = {
   measurementId: "G-0LQQQ1TBYP"
 };
 
-
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
 const analytics = getAnalytics(app);
 
-
-// Инициализация Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// Загрузка товаров
 const productsContainer = document.getElementById("products");
 
 function renderProduct(doc) {
@@ -52,14 +35,12 @@ function renderProduct(doc) {
   productsContainer.appendChild(productDiv);
 }
 
-// Реальное время: слушаем изменения в базе
 db.collection("products").onSnapshot(snapshot => {
   productsContainer.innerHTML = "";
   snapshot.forEach(doc => {
     renderProduct(doc);
   });
 });
-// Регистрация service worker
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js")
     .then(() => console.log("Service Worker зарегистрирован"))
--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Подключаем папку public для статики
 app.use(express.static(path.join(__dirname, "public")));
 
-// Отдаём index.html при заходе на /
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
